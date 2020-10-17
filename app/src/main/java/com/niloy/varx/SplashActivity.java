1463ad37315fcb2d9cef024dabd86ba861240e45
saiful-islam-niloy <saifulislam65@iut-dhaka.edu>
2020-10-17 16:23:14 +0600

    home activity added

--- app/src/main/java/com/niloy/varx/SplashActivity.java
+++ app/src/main/java/com/niloy/varx/SplashActivity.java
@@ -3,6 +3,7 @@ package com.niloy.varx;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class SplashActivity extends AppCompatActivity {
@@ -14,9 +15,28 @@ public class SplashActivity extends AppCompatActivity {
 
         hideActionBar();
 
+        runSplashThread();
+
     }
     private void hideActionBar(){
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
+    }
+
+    private void runSplashThread(){
+        Thread myThread = new Thread(){
+            @Override
+            public void run() {
+                try {
+                    sleep(1000);
+                    Intent intent = new Intent(getApplicationContext(),HomeActivity.class);
+                    startActivity(intent);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        myThread.start();
     }
 }
