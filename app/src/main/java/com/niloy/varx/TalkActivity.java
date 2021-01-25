ef950822bc354582d4b22ddf7065db68668fab63
saiful-islam-niloy <saifulislam65@iut-dhaka.edu>
2021-01-25 15:59:50 +0600

    voice-text

--- app/src/main/java/com/niloy/varx/TalkActivity.java
+++ app/src/main/java/com/niloy/varx/TalkActivity.java
@@ -1,42 +1,174 @@
 package com.niloy.varx;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Locale;
 
 public class TalkActivity extends AppCompatActivity {
     private ImageButton mic;
+    private TextView text;
     private boolean isMicOn = false;
+    private static final Integer RecordAudioRequestCode = 1;
+    private SpeechRecognizer speechRecognizer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_talk);
 
-        hideActionBar();
+        initializeUI();
+        checkPermission();
+        initializeSpeechRecogniser();
 
-        mic = findViewById(R.id.mic);
-        mic.setOnClickListener(new View.OnClickListener() {
+        speechRecognizer = SpeechRecognizer.createSpeechRecognizer(this);
+
+        final Intent speechRecognizerIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        speechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        speechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+
+        speechRecognizer.setRecognitionListener(new RecognitionListener() {
             @Override
-            public void onClick(View v) {
-                if(!isMicOn){
-                    mic.setImageResource(R.drawable.mic_on);
-                    mic.setBackgroundColor(getResources().getColor(R.color.purple_500));
-                    isMicOn = true;
-                }else {
-                    mic.setImageResource(R.drawable.mic_off);
-                    mic.setBackgroundColor(getResources().getColor(R.color.lightGray));
-                    isMicOn = false;
-                }
+            public void onReadyForSpeech(Bundle bundle) {
+
+            }
+
+            @Override
+            public void onBeginningOfSpeech() {
+                text.setText("");
+                text.setHint("Listening...");
+            }
+
+            @Override
+            public void onRmsChanged(float v) {
+
+            }
+
+            @Override
+            public void onBufferReceived(byte[] bytes) {
+
+            }
+
+            @Override
+            public void onEndOfSpeech() {
+
+            }
+
+            @Override
+            public void onError(int i) {
+
+            }
+
+            @Override
+            public void onResults(Bundle bundle) {
+                mic.setImageResource(R.drawable.mic_off);
+                mic.setBackgroundColor(getResources().getColor(R.color.lightGray));
+                ArrayList<String> data = bundle.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+                text.setText(data.get(0));
+            }
+
+            @Override
+            public void onPartialResults(Bundle bundle) {
+
+            }
+
+            @Override
+            public void onEvent(int i, Bundle bundle) {
+
             }
         });
+
+        mic = findViewById(R.id.mic);
+        mic.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                if (motionEvent.getAction() == MotionEvent.ACTION_UP) {
+                    mic.setImageResource(R.drawable.mic_off);
+                    mic.setBackgroundColor(getResources().getColor(R.color.purple_500));
+                    speechRecognizer.stopListening();
+                }
+                if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+                    mic.setImageResource(R.drawable.mic_on);
+                    mic.setBackgroundColor(getResources().getColor(R.color.lightGray));
+                    speechRecognizer.startListening(speechRecognizerIntent);
+                }
+                return false;
+            }
+        });
+//        mic.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                if(isMicOn){
+//                    speechRecognizer.stopListening();
+//                    mic.setImageResource(R.drawable.mic_off);
+//                    mic.setBackgroundColor(getResources().getColor(R.color.lightGray));
+//                    isMicOn = false;
+//                }else {
+//                    speechRecognizer.startListening(speechRecognizerIntent);
+//                    mic.setImageResource(R.drawable.mic_on);
+//                    mic.setBackgroundColor(getResources().getColor(R.color.purple_500));
+//                    isMicOn = true;
+//                }
+//            }
+//        });
+    }
+
+    private void initializeUI() {
+        text = findViewById(R.id.voice_to_text);
+        hideActionBar();
+    }
+
+    private void initializeSpeechRecogniser() {
+        speechRecognizer = SpeechRecognizer.createSpeechRecognizer(this);
+        final Intent speechRecognizerIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        speechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        speechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+    }
+
+    private void checkPermission() {
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED){
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.RECORD_AUDIO},RecordAudioRequestCode);
+            }
+        }
     }
 
 
     private void hideActionBar(){
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        speechRecognizer.destroy();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == RecordAudioRequestCode && grantResults.length > 0 ){
+            if(grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                Toast.makeText(this,"Permission Granted", Toast.LENGTH_SHORT).show();
+        }
     }
 }
